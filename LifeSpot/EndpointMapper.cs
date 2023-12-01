14ad322673dddfe64bdec00ddd81a0da2ef52dc1
Александр Лещинский <topjumbo@mail.ru>
2023-12-01 17:43:41 +1000

    добавил слайдер

---
+++ LifeSpot/EndpointMapper.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using System.IO;
+using System.Text;
+
+namespace LifeSpot
+{
+    public static class EndpointMapper
+    {
+        /// <summary>
+        ///  Маппинг CSS-файлов
+        /// </summary>
+        public static void MapCss(this IEndpointRouteBuilder builder)
+        {
+            var cssFiles = new[] { "index.css" };
+
+            foreach (var fileName in cssFiles)
+            {
+                builder.MapGet($"/Static/CSS/{fileName}", async context =>
+                {
+                    var cssPath = Path.Combine(Directory.GetCurrentDirectory(), "Static", "CSS", fileName);
+                    var css = await File.ReadAllTextAsync(cssPath);
+                    await context.Response.WriteAsync(css);
+                });
+            }
+        }
+
+        /// <summary>
+        ///  Маппинг JS
+        /// </summary>
+        public static void MapJs(this IEndpointRouteBuilder builder)
+        {
+            var jsFiles = new[] { "index.js", "testing.js", "about.js" };
+
+            foreach (var fileName in jsFiles)
+            {
+                builder.MapGet($"/Static/JS/{fileName}", async context =>
+                {
+                    var jsPath = Path.Combine(Directory.GetCurrentDirectory(), "Static", "JS", fileName);
+                    var js = await File.ReadAllTextAsync(jsPath);
+                    await context.Response.WriteAsync(js);
+                });
+            }
+        }
+
+        /// <summary>
+        ///  Маппинг Html-страниц
+        /// </summary>
+        public static void MapHtml(this IEndpointRouteBuilder builder)
+        {
+            string footerHtml = File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(), "Views", "Shared", "footer.html"));
+            string sideBarHtml = File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(), "Views", "Shared", "sidebar.html"));
+            string sliderHtml = File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(), "Views", "Shared", "slider.html"));
+
+            builder.MapGet("/", async context =>
+            {
+                var viewPath = Path.Combine(Directory.GetCurrentDirectory(), "Views", "index.html");
+                var viewText = await File.ReadAllTextAsync(viewPath);
+
+                // Загружаем шаблон страницы, вставляя в него элементы
+                var html = new StringBuilder(await File.ReadAllTextAsync(viewPath))
+                    .Replace("<!--SIDEBAR-->", sideBarHtml)
+                    .Replace("<!--FOOTER-->", footerHtml);
+
+                await context.Response.WriteAsync(html.ToString());
+            });
+
+            builder.MapGet("/testing", async context =>
+            {
+                var viewPath = Path.Combine(Directory.GetCurrentDirectory(), "Views", "testing.html");
+
+                // Загружаем шаблон страницы, вставляя в него элементы
+                var html = new StringBuilder(await File.ReadAllTextAsync(viewPath))
+                    .Replace("<!--SIDEBAR-->", sideBarHtml)
+                    .Replace("<!--FOOTER-->", footerHtml);
+
+                await context.Response.WriteAsync(html.ToString());
+            });
+
+            builder.MapGet("/about", async context =>
+            {
+                var viewPath = Path.Combine(Directory.GetCurrentDirectory(), "Views", "about.html");
+
+                // Загружаем шаблон страницы, вставляя в него элементы
+                var html = new StringBuilder(await File.ReadAllTextAsync(viewPath))
+                    .Replace("<!--SIDEBAR-->", sideBarHtml)
+                    .Replace("<!--SLIDER-->", sliderHtml)
+                    .Replace("<!--FOOTER-->", footerHtml);
+                    
+                await context.Response.WriteAsync(html.ToString());
+            });
+        }
+
+        /// <summary>
+        ///  Маппинг IMG
+        /// </summary>
+        public static void MapIMG(this IEndpointRouteBuilder builder)
+        {
+            var imgFiles = new[] { "img_mountains_wide.jpg", "img_nature_wide.jpg", "img_snow_wide.jpg" };
+
+            foreach (var fileName in imgFiles)
+            {
+                builder.MapGet($"/Static/IMG/{fileName}", async context =>
+                {
+                    var imgPath = Path.Combine(Directory.GetCurrentDirectory(), "Static", "IMG", fileName);
+                    var img = await File.ReadAllBytesAsync(imgPath);
+                    await context.Response.Body.WriteAsync(img);
+                });
+            }
+        }
+
+    }
+}
